ad522dcf1559b386b8c170b86903eb900675b610
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-07 13:29:43 -0800

    ww

--- fourth_file.c
+++ fourth_file.c
@@ -22,3 +22,46 @@ char *my_copy_string(char *destination, char *source)
 	return (destination);
 }
 
+
+/**
+ * my_duplicate_string - Duplicates a given string.
+ *
+ * @str: The string to duplicate.
+ * Return: Pointer to the duplicated string.
+ */
+char *my_duplicate_string(const char *str)
+{
+	int length = 0;
+	char *result;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	result = malloc(sizeof(char) * (length + 1));
+	if (!result)
+		return (NULL);
+	for (length++; length--;)
+		result[length] = *--str;
+	return (result);
+}
+
+
+/**
+ * my_print_string - Prints a string to stdout.
+ * @str: The string to be printed.
+ *
+ * Return: Nothing.
+ */
+void my_print_string(char *str)
+{
+	int index = 0;
+
+	if (!str)
+		return;
+	while (str[index] != '\0')
+	{
+		my_putchar(str[index]);
+		index++;
+	}
+}
