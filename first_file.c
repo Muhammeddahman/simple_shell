2611e1043a5745c85f3ed36dd2bd476d3e4de5a2
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-07 13:09:55 -0800

    ww

--- first_file.c
+++ first_file.c
@@ -18,3 +18,28 @@ int custom_strlen(char *s)
 
 	return (length);
 }
+
+
+/**
+ * custom_strcmp - Perform a lexicographic comparison of two strings.
+ *
+ * @s1: The first string.
+ * @s2: The second string.
+ * Return: Negative if s1 < s2, positive if s1 > s2, zero if s1 == s2.
+ */
+int custom_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+		return (0);
+	else
+		return ((*s1 < *s2) ? -1 : 1);
+}
