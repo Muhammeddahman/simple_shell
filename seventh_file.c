f119e8b028294769f04560fcaabef2edf23745ab
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-08 11:03:19 -0800

    ww

--- seventh_file.c
+++ seventh_file.c
@@ -22,3 +22,23 @@ int isExecutableCommand(info_t *info, char *path)
 	return (0);
 }
 
+/**
+ * duplicateCharacters - Duplicates characters in a given range
+ * @pathStr: The PATH string
+ * @start: Starting index
+ * @stop: Stopping index
+ *
+ * Return: Pointer to a new buffer containing the duplicated characters
+ */
+char *duplicateCharacters(char *pathStr, int start, int stop)
+{
+	static char buffer[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathStr[i] != ':')
+			buffer[k++] = pathStr[i];
+	buffer[k] = '\0';
+	return (buffer);
+}
+
