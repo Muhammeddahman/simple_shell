77fdef2e6e878758f48277f576464a7711410b79
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-08 11:10:18 -0800

    ww

--- eightth_file.c
+++ eightth_file.c
@@ -38,3 +38,32 @@ int isAlphabetic(int c)
 		return (0);
 }
 
+/**
+ * convertStringToInt - Converts a string to an integer
+ * @s: The string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
+int convertStringToInt(char *s)
+{
+	int i, sign = 1, flag = 0, result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		return (-result);
+	else
+		return (result);
+}
