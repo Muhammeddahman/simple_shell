4a7bd20005652847e7c8e4677fd2cbcdef4ba2a2
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-09 12:23:43 -0800

    ww

---
+++ tenth_file.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+/**
+ * primaryFunction - The main functionality of the program
+ * @argumentCount: Number of arguments
+ * @argumentVector: Argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
