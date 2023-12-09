7d2e8c3c2f2af0d8b61eec1a570c2c905c97078a
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-09 12:50:48 -0800

    ww

--- twelveth_file.c
+++ twelveth_file.c
@@ -82,3 +82,65 @@ list_t *add_node_end(list_t **head, const char *data, int index)
 	return (new_node);
 }
 
+/**
+ * print_list_str - Prints only the str element of a list_t linked list.
+ * @h: Pointer to the first node.
+ *
+ * Return: Size of the list.
+ */
+size_t print_list_str(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * delete_node_at_index - Deletes a node at the specified index.
+ * @head: Address of the pointer to the first node.
+ * @index: Index of the node to delete.
+ *
+ * Return: 1 on success, 0 on failure.
+ */
+int delete_node_at_index(list_t **head, unsigned int index)
+{
+	list_t *node, *prev_node;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (0);
+
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node->str);
+		free(node);
+		return (1);
+	}
+
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node->str);
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node;
+		node = node->next;
+	}
+
+	return (0);
+}
+
