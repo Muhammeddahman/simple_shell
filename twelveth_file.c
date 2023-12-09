223e54bd0f726aa03930690e76c2166ba59cd73e
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-09 12:38:36 -0800

    ww

--- twelveth_file.c
+++ twelveth_file.c
@@ -37,3 +37,50 @@ list_t *prependNode(list_t **head, const char *data, int index)
 	return (newNode);
 }
 
+/**
+ * appendNode - Adds a node to the end of the linked list
+ * @head: Address of the pointer to the head node
+ * @data: Data field of the node
+ * @index: Index used for history
+ *
+ * Return: Pointer to the newly added node
+ */
+list_t *appendNode(list_t **head, const char *data, int index)
+{
+	list_t *newNode, *node;
+
+	if (!head)
+		return (NULL);
+
+	node = *head;
+	newNode = malloc(sizeof(list_t));
+
+	if (!newNode)
+		return (NULL);
+
+	_memset((void *)newNode, 0, sizeof(list_t));
+	newNode->index = index;
+
+	if (data)
+	{
+		newNode->data = _strdup(data);
+		if (!newNode->data)
+		{
+			free(newNode);
+			return (NULL);
+		}
+	}
+
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+
+		node->next = newNode;
+	}
+	else
+		*head = newNode;
+
+	return (newNode);
+}
+
