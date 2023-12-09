e2ff81605b2a19881dbf88a48067169fedd0b377
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-09 12:21:14 -0800

    ww

--- ninth_file.c
+++ ninth_file.c
@@ -119,3 +119,20 @@ char *convertToBase(long int num, int base, int flags)
 	return (ptr);
 }
 
+/**
+ * eliminateComments - Function replaces the first instance of '#' with '\0'
+ * @buf: Address of the string to modify
+ *
+ * Return: Always 0
+ */
+void eliminateComments(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
