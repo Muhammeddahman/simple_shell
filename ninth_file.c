9521078d06e4bc94b705fb23543829d93c4a5146
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-09 12:20:37 -0800

    ww

--- ninth_file.c
+++ ninth_file.c
@@ -83,3 +83,39 @@ int displayDecimal(int input, int fd)
 	return (count);
 }
 
+/**
+ * convertToBase - Converter function, a clone of itoa
+ * @num: Number
+ * @base: Base
+ * @flags: Argument flags
+ *
+ * Return: String
+ */
+char *convertToBase(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
