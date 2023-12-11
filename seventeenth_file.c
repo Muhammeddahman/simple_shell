fffb40a0db85dfe18c51caa4ab55c271fd274a28
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-11 12:57:17 -0800

    ww

--- seventeenth_file.c
+++ seventeenth_file.c
@@ -43,3 +43,28 @@ int _putToStderr(char c)
 	return (1);
 }
 
+
+/**
+ * _putToFD - Writes a character to a given file descriptor
+ * @c: The character to print
+ * @fd: The file descriptor to write to
+ *
+ * Return:success 1. On error,-1 is returned, and errno is set appropriately.
+ */
+int _putToFD(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+
+	return (1);
+}
+
