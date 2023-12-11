d9bbc44bb1aa390fbb546f6a1c8a3bbd37f756af
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-11 13:36:30 -0800

    ww

--- twentieth_file.c
+++ twentieth_file.c
@@ -69,3 +69,73 @@ void checkChain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 	*p = j;
 }
 
+/**
+ * replaceAlias - Replaces aliases in the tokenized string
+ * @info: The parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replaceAlias(info_t *info)
+{
+	int i;
+	list_t *node;
+	char *p;
+
+	for (i = 0; i < 10; i++)
+	{
+		node = nodeStartsWith(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = _strchr(node->str, '=');
+		if (!p)
+			return (0);
+		p = _strdup(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
+	return (1);
+}
+
+/**
+ * replaceVariables - Replaces variables in the tokenized string
+ * @info: The parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replaceVariables(info_t *info)
+{
+	int i = 0;
+	list_t *node;
+
+	for (i = 0; info->argv[i]; i++)
+	{
+		if (info->argv[i][0] != '$' || !info->argv[i][1])
+			continue;
+
+		if (!_strcmp(info->argv[i], "$?"))
+		{
+			replaceString(&(info->argv[i]),
+				_strdup(convertNumber(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[i], "$$"))
+		{
+			replaceString(&(info->argv[i]),
+				_strdup(convertNumber(getpid(), 10, 0)));
+			continue;
+		}
+		node = nodeStartsWith(info->env, &info->argv[i][1], '=');
+		if (node)
+		{
+			replaceString(&(info->argv[i]),
+				_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replaceString(&info->argv[i], _strdup(""));
+
+	}
+	return (0);
+}
+
