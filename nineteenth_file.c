b7ebf6922b2a3726bec496487a1c6ce98d10e4c9
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-11 13:26:18 -0800

    ww

--- nineteenth_file.c
+++ nineteenth_file.c
@@ -12,3 +12,25 @@ int _printEnv(info_t *info)
 	return (0);
 }
 
+/**
+ * _retrieveEnvVar - Gets the value of an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: Environment variable name
+ *
+ * Return: The value of the environment variable
+ */
+char *_retrieveEnvVar(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = startsWith(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
