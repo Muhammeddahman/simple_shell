df158390f4a11ff02750e8f00be77d84ee0df7d5
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-11 13:20:37 -0800

    ww

--- eighteenth_file.c
+++ eighteenth_file.c
@@ -49,3 +49,45 @@ int removeEnvironmentVar(info_t *info, char *var)
 	return (info->envChanged);
 }
 
+/**
+ * setEnvironmentVar - Initializes a new environment variable
+ *                     or modifies an existing one
+ * @info: The structure holding potential arguments. Used for maintaining
+ *        a constant function prototype.
+ * @var: The string property of the environment variable
+ * @value: The string value of the environment variable
+ * Return: Always 0
+ */
+int setEnvironmentVar(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = startsWith(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->envChanged = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	addNodeEnd(&(info->env), buf, 0);
+	free(buf);
+	info->envChanged = 1;
+	return (0);
+}
