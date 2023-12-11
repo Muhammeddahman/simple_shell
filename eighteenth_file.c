8d8d0fc28013c96a58f4abe68fa5af7dbb3012bd
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-11 13:19:43 -0800

    ww

--- eighteenth_file.c
+++ eighteenth_file.c
@@ -17,3 +17,35 @@ char **fetchEnvironment(info_t *info)
 	return (info->environ);
 }
 
+/**
+ * removeEnvironmentVar - Removes an environment variable
+ * @info: The structure holding potential arguments. Used for maintaining
+ *        a constant function prototype.
+ * @var: The string property of the environment variable
+ * Return: 1 on delete, 0 otherwise
+ */
+int removeEnvironmentVar(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = startsWith(node->str, var);
+		if (p && *p == '=')
+		{
+			info->envChanged = deleteNodeAtIndex(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->envChanged);
+}
+
