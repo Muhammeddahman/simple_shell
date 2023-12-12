19fd3118f69c4bddbed2b916c9b75200c9cf419b
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-12 11:04:36 -0800

    ww

--- twentyoneth_file.c
+++ twentyoneth_file.c
@@ -82,4 +82,36 @@ int disAlias(list_t *node)
 	}
 	return (1);
 }
+/**
+ * _shAlias - Placeholder function for an alias-related operation.
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Placeholder return value
+ */
+int _shAlias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
 
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			disAlias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			assAlias(info, info->argv[i]);
+		else
+			disAlias(nodeStartsWith(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
