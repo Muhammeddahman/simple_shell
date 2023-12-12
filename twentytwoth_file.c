73d8ebeb71e54979b285df906c887167e5838396
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-12 11:52:33 -0800

    ww

--- twentytwoth_file.c
+++ twentytwoth_file.c
@@ -1,10 +1,12 @@
 #include "shell.h"
 
+
+
 /**
- * _customExit - exits the shell with a given exit status.
+ * _customExit - Exits the shell with a given exit status.
  * @info: Structure containing potential arguments.
- * Return: Exits with a specified exit status.
- *         (0) if info.argv[0] != "exit".
+ *
+ * Return: Exits with a specified exit status (0 if info.argv[0] != "exit").
  */
 int _customExit(info_t *info)
 {
@@ -24,7 +26,59 @@ int _customExit(info_t *info)
 		info->err_num = exitCode;
 		return (-2);
 	}
+
 	info->err_num = -1;
 	return (-2);
+}
+
+
+
+#include "shell.h"
+
+/**
+ * _customCd - Changes the current directory of the process.
+ * @info: Structure containing potential arguments.
+ *
+ * Return: Always 0.
+ */
+int _customCd(info_t *info)
+{
+	char *current_dir, *new_dir, buffer[1024];
+	int chdir_result;
+
+	current_dir = getcwd(buffer, 1024);
+	if (!current_dir)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+
+	if (!info->argv[1])
+	{
+		new_dir = _getenv(info, "HOME=");
+		if (!new_dir)
+			chdir_result = chdir((new_dir = _getenv(info, "PWD=")) ? new_dir : "/");
+		else
+			chdir_result = chdir(new_dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+			return (_puts(current_dir), _putchar('\n'), 1);
+
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_result = chdir((new_dir = _getenv(info, "OLDPWD=")) ? new_dir : "/");
+	}
+	else
+		chdir_result = chdir(info->argv[1]);
+
+	if (chdir_result == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
 }
 
