26371ddb9253d807f007e70b2797719c04a5dfcd
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-12 10:59:51 -0800

    ww

--- twentyoneth_file.c
+++ twentyoneth_file.c
@@ -14,3 +14,72 @@ int _shHistory(info_t *info)
 	return (0);
 }
 
+/**
+ * remAlias - Removes an alias from the alias list.
+ * @info: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ * @str: String parameter representing the alias to be removed.
+ * Return: No return value
+ */
+int remAlias(info_t *info, char *str)
+{
+	char *p, c;
+	int ret;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = deleteNodeAtIndex(&(info->alias),
+		getNodeIndex(info->alias, nodeStartsWith(info->alias, str, -1)));
+	*p = c;
+	return (ret);
+}
+
+
+/**
+ * assAlias - Assigns an alias to a specified string.
+ * @info: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ * @str: String parameter representing the alias to be assigned.
+ * Return: No return value
+ */
+
+int assAlias(info_t *info, char *str)
+{
+	char *p;
+
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	if (!*++p)
+		return (remAlias(info, str));
+
+	remAlias(info, str);
+	return (addNodeEnd(&(info->alias), str, 0) == NULL);
+}
+
+
+/**
+ * disAlias - Displays the aliases.
+ * @node: Node containing the alias information.
+ * Return: No return value
+ */
+int disAlias(list_t *node)
+{
+	char *p = NULL, *a = NULL;
+
+	if (node)
+	{
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+			_putchar(*a);
+		_putchar('\'');
+		_puts(p + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
