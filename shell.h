86ddbb7db05580befde596dd3393403dcdc4d19e
Muhammeddahman <mohamedyasserdahman@gmail.com>
2023-12-13 14:52:11 -0800

    ww

--- shell.h
+++ shell.h
@@ -12,106 +12,104 @@
 #include <fcntl.h>
 #include <errno.h>
 
-/* Read/write buffer sizes */
+/* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-/* Command chaining types */
-#define CMD_NORM 0
-#define CMD_OR 1
-#define CMD_AND 2
-#define CMD_CHAIN 3
+/* for command chaining */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
 
-/* Convert number options */
-#define CONVERT_LOWERCASE 1
-#define CONVERT_UNSIGNED 2
+/* for convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
 
-/* System getline usage flag */
+/* 1 if using system getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
-/* History file and max size */
-#define HIST_FILE ".simple_shell_history"
-#define HIST_MAX 4096
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
 
 extern char **environ;
 
+
 /**
- * struct liststr - Singly linked list
- * @my_num: Number field
- * @my_str: String
- * @my_next: Points to the next node
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
  */
 typedef struct liststr
 {
-	int my_num;
-	char *my_str;
-	struct liststr *my_next;
+	int num;
+	char *str;
+	struct liststr *next;
 } list_t;
 
 /**
- * struct info - Contains pseudo-arguments to pass into a function,
- *               allowing a uniform prototype for a function pointer struct
- * @String: A string generated from getline containing arguments
- * @argv: An array of strings generated from String
- * @path: A string path for the current command
- * @argc: The argument count
- * @lineCount: The error count
- * @errNum: The error code for exit()s
- * @lineCountFlag: If on count this line of input
- * @fileName: The program filename
- * @envList: Linked list local copy of environ
- * @customEnv: Custom modified copy of environ from Linked List envList
- * @historyList: The history node
- * @aliasList: The alias node
- * @envChanged: On if environ was changed
- * @returnStatus: The return status of the last exec'd command
- * @cmdBuffer: Address of pointer to cmd_buffer, on if chaining
- * @cmdBufferType: CMD_type ||, &&, ;
- * @readFileDescriptor: The file descriptor from which to read line input
- * @historyCount: The history line number count
+ *struct passinfo - contains pseudo-arguements to pass into a function,
+ *					allowing uniform prototype for function pointer struct
+ *@arg: a string generated from getline containing arguements
+ *@argv: an array of strings generated from arg
+ *@path: a string path for the current command
+ *@argc: the argument count
+ *@line_count: the error count
+ *@err_num: the error code for exit()s
+ *@linecount_flag: if on count this line of input
+ *@fname: the program filename
+ *@env: linked list local copy of environ
+ *@environ: custom modified copy of environ from LL env
+ *@history: the history node
+ *@alias: the alias node
+ *@env_changed: on if environ was changed
+ *@status: the return status of the last exec'd command
+ *@cmd_buf: address of pointer to cmd_buf, on if chaining
+ *@cmd_buf_type: CMD_type ||, &&, ;
+ *@readfd: the fd from which to read line input
+ *@histcount: the history line number count
  */
-typedef struct info
+typedef struct passinfo
 {
-	char *String;
+	char *arg;
 	char **argv;
 	char *path;
 	int argc;
-	unsigned int lineCount;
-	int errNum;
-	int lineCountFlag;
-	char *fileName;
-	list_t *envList;
-	list_t *historyList;
-	list_t *aliasList;
-	char **customEnv;
-	int envChanged;
-	int returnStatus;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
 
-	char **cmdBuffer; /* Pointer to cmd ; chain buffer, for memory management */
-	int cmdBufferType; /* CMD_type ||, &&, ; */
-	int readFileDescriptor;
-	int historyCount;
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
 } info_t;
 
-#define INFO_INIT                           \
-	{                                    \
-	NULL, NULL, NULL, 0, 0, 0, 0, NULL, \
-	NULL, NULL, NULL, NULL, NULL, 0, 0,  \
-	NULL, 0, 0, 0                      \
-	}
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
 
 /**
- * struct builtin - Contains a builtin string and related function
- * @my_type: The builtin command flag
- * @my_func: The function
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
  */
 typedef struct builtin
 {
-	char *my_type;
-	int (*my_func)(info_t *);
+	char *type;
+	int (*func)(info_t *);
 } builtin_table;
+
 
 /* Function declarations (with changed comments) */
 
